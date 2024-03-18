a49ff5b8f496f4e4dbf8a4e6ea2a43f30575abcd
Burak Sezer <burak.sezer.developer@gmail.com>
2024-03-18 11:19:30 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/TykTechnologies/graphql-translator
+
+go 1.21.0
+
+toolchain go1.21.6
+
+require (
+	github.com/TykTechnologies/graphql-go-tools v1.6.2-0.20240315095341-b464dd4dc1ce
+	github.com/getkin/kin-openapi v0.89.0
+	github.com/iancoleman/strcase v0.2.0
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.5 // indirect
+	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ openapi/openapi.go
@@ -0,0 +1,155 @@
+package openapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+
+	"github.com/TykTechnologies/graphql-go-tools/pkg/ast"
+	"github.com/TykTechnologies/graphql-go-tools/pkg/introspection"
+	"github.com/TykTechnologies/graphql-go-tools/pkg/operationreport"
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+var (
+	errTypeNameExtractionImpossible = errors.New("type name extraction is impossible")
+	errNotPrimitiveType             = errors.New("not a primitive type")
+)
+
+type converter struct {
+	openapi         *openapi3.T
+	knownFullTypes  map[string]*knownFullTypeDetails
+	knownEnums      map[string]introspection.FullType
+	knownUnions     map[string]introspection.FullType
+	fullTypes       []introspection.FullType
+	currentPathName string
+	currentPathItem *openapi3.PathItem
+}
+
+type knownFullTypeDetails struct {
+	hasDescription bool
+}
+
+func newConverter(document *openapi3.T) *converter {
+	return &converter{
+		openapi:        document,
+		knownFullTypes: make(map[string]*knownFullTypeDetails),
+		knownEnums:     make(map[string]introspection.FullType),
+		knownUnions:    make(map[string]introspection.FullType),
+		fullTypes:      make([]introspection.FullType, 0),
+	}
+}
+
+// ImportParsedOpenAPIv3Document imports a parsed OpenAPI v3 document and converts it into a GraphQL AST Document.
+func ImportParsedOpenAPIv3Document(document *openapi3.T, report *operationreport.Report) *ast.Document {
+	c := newConverter(document)
+	data := introspection.Data{}
+
+	queryType, err := c.importQueryType()
+	if err != nil {
+		report.AddInternalError(err)
+		return nil
+	}
+	if len(queryType.Fields) > 0 {
+		data.Schema.QueryType = &introspection.TypeName{
+			Name: "Query",
+		}
+		data.Schema.Types = append(data.Schema.Types, *queryType)
+	} else {
+		data.Schema.QueryType = &introspection.TypeName{
+			Name: "QueryPlaceholder",
+		}
+		stringScalarType := "String"
+		placeholderObject := introspection.FullType{
+			Kind:        introspection.OBJECT,
+			Name:        "QueryPlaceholder",
+			Description: "Placeholder object",
+			Fields: []introspection.Field{
+				{
+					Name:        "message",
+					Description: "Placeholder field",
+					Type:        introspection.TypeRef{Kind: introspection.SCALAR, Name: &stringScalarType},
+				},
+			},
+		}
+		data.Schema.Types = append(data.Schema.Types, placeholderObject)
+	}
+
+	mutationType, err := c.importMutationType()
+	if err != nil {
+		report.AddInternalError(err)
+		return nil
+	}
+	if len(mutationType.Fields) > 0 {
+		data.Schema.MutationType = &introspection.TypeName{
+			Name: "Mutation",
+		}
+		data.Schema.Types = append(data.Schema.Types, *mutationType)
+	}
+
+	fullTypes, err := c.importFullTypes()
+	if err != nil {
+		report.AddInternalError(err)
+		return nil
+	}
+	data.Schema.Types = append(data.Schema.Types, fullTypes...)
+
+	outputPretty, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		report.AddInternalError(err)
+		return nil
+	}
+
+	jc := introspection.JsonConverter{}
+	buf := bytes.NewBuffer(outputPretty)
+	doc, err := jc.GraphQLDocument(buf)
+	if err != nil {
+		report.AddInternalError(err)
+		return nil
+	}
+	return doc
+}
+
+// ParseOpenAPIDocument parses an OpenAPI document from a byte slice input.
+// It creates a new loader with IsExternalRefsAllowed set to true.
+// The loader then loads the document from the input data, returning any errors encountered.
+// If the document is successfully loaded, it is validated using the loader's context.
+// If validation fails, the validation error is returned.
+// Otherwise, the parsed document is returned along with a nil error.
+func ParseOpenAPIDocument(input []byte) (*openapi3.T, error) {
+	loader := openapi3.NewLoader()
+	loader.IsExternalRefsAllowed = true
+	document, err := loader.LoadFromData(input)
+	if err != nil {
+		return nil, err
+	}
+	if err = document.Validate(loader.Context); err != nil {
+		return nil, err
+	}
+	return document, nil
+}
+
+// ImportOpenAPIDocumentByte imports an OpenAPI document from a byte slice input.
+// It first parses the input using the ParseOpenAPIDocument function.
+// If there is an error during parsing, it adds the error to the operationreport.Report
+// and returns nil with the report.
+// Otherwise, it passes the parsed document to the ImportParsedOpenAPIv3Document function
+// along with the operationreport.Report, and returns the imported OpenAPI document
+// as ast.Document and the report.
+func ImportOpenAPIDocumentByte(input []byte) (*ast.Document, operationreport.Report) {
+	report := operationreport.Report{}
+	document, err := ParseOpenAPIDocument(input)
+	if err != nil {
+		report.AddInternalError(err)
+		return nil, report
+	}
+	return ImportParsedOpenAPIv3Document(document, &report), report
+}
+
+// ImportOpenAPIDocumentString imports an OpenAPI document from a string input.
+// It delegates to ImportOpenAPIDocumentByte, passing the input as a byte slice.
+// The function returns the imported OpenAPI document as ast.Document and an operationreport.Report.
+// If there are any errors during the import process, they will be included in the report.
+func ImportOpenAPIDocumentString(input string) (*ast.Document, operationreport.Report) {
+	return ImportOpenAPIDocumentByte([]byte(input))
+}
